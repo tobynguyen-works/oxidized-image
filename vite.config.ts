9aa0bcb750899eef3a2c95906792d3195121b2c5
Toby Nguyen <tobynguyen27@proton.me>
2026-07-11 19:17:32 +0700

    chore: update Vite+ task

--- vite.config.ts
+++ vite.config.ts
@@ -13,15 +13,15 @@ export default defineConfig({
 			},
 			test: {
 				command: 'vp run -r test',
-				dependsOn: ['build', 'check'],
+				dependsOn: ['check', { task: 'build', from: ['dependencies', 'devDependencies'] }],
 			},
 			play: {
 				command: 'vp run -r play',
-				dependsOn: ['build'],
+				dependsOn: [{ task: 'build', from: ['dependencies', 'devDependencies'] }],
 			},
 			check: {
 				command: 'vp check',
-				dependsOn: ['build'],
+				dependsOn: [{ task: 'build', from: ['dependencies', 'devDependencies'] }],
 			},
 		},
 	},
